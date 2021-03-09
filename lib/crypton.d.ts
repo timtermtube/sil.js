a71952f86bed6b870a594a8a91654bccdef60659
timtermtube <juhyeong0221@daum.net>
2021-03-09 22:15:02 +0900

    Update Files

--- lib/crypton.d.ts
+++ lib/crypton.d.ts
@@ -1,7 +1,7 @@
 declare module "crypton" {
-    function encrypt(string: string): Array;
+    function encrypt(string: string): Array<Number>;
     
-    function decrypt(array: Array): string;
+    function decrypt(array: Array<Number>): string;
 
     export {encrypt, decrypt};
 }
