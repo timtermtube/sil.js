1281c8d443b1212588fb9331d307a67ec709005e
timtermtube <juhyeong0221@daum.net>
2021-03-13 01:18:20 +0900

    Add Client-Side crypt

--- src/ThreadWorker.js
+++ src/ThreadWorker.js
@@ -1,6 +1,26 @@
 /* ThreadWorker */
 
-import { encrypt, decrypt } from "./SilCrypt"
+function encrypt(string) {
+    const array = [];
+
+    if (typeof string == "string") {
+        for (var i=0; i<string.length; i++) {
+            array.push(string.charCodeAt(i)*2)
+        }
+    }
+    return array;
+}
+
+function decrypt(array) {
+    let string = "";
+
+    if (typeof array == "object") {
+        for (var i=0; i<array.length; i++) {
+            string += String.fromCharCode(array[i]/2)
+        }
+    }
+    return string;
+}
 
 const ThreadWorker = (function(){
     self.__Worker = true;
